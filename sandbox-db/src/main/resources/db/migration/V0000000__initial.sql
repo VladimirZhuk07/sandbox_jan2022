27074b3b8ddb795cad42f5ee8138337410298f43
Ilya Getman <vieletiger@gmail.com>
2022-02-03 17:52:10 +0300

    add user/role/vacation sql script

--- sandbox-db/src/main/resources/db/migration/V0000000__initial.sql
+++ sandbox-db/src/main/resources/db/migration/V0000000__initial.sql
@@ -1,7 +1,36 @@
 CREATE TABLE `User`
 (
-   id         INT                        NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   username   VARCHAR(50)                NOT NULL,
+   id                   INT                        NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   chat_id              INT                        NOT NULL,
+   first_name           VARCHAR(50)                NOT NULL,
+   last_name            VARCHAR(50)                NOT NULL,
+   email                VARCHAR(50)                NOT NULL,
+   employment_start     DATE                       NOT NULL,
+   employment_end       DATE,
+   is_fired             BOOLEAN,
+   created_date         DATE                       NOT NULL
+);
 
-   UNIQUE KEY `username` (`username`)
+CREATE TABLE `Vacation`
+(
+   id                   INT                        NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   user_id              INT                        NOT NULL,
+   vacation_start       DATE                       NOT NULL,
+   vacation_end         DATE                       NOT NULL,
+   FOREIGN KEY (user_id) REFERENCES User(id)
+);
+
+CREATE TABLE `Role`
+(
+   id                   INT                        NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   name                 VARCHAR(50)                NOT NULL
+);
+
+CREATE TABLE `User_role`
+(
+   user_id              INT                        NOT NULL,
+   role_id              INT                        NOT NULL,
+   PRIMARY KEY (user_id, role_id),
+   FOREIGN KEY (user_id) REFERENCES User(id),
+   FOREIGN KEY (role_id) REFERENCES Role(id)
 );
