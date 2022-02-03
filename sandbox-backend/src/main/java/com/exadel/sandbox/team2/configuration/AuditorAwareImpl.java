2b8fbd965bf50a1cdbc5f94bdc6a478ee5753e56
Azizbek <mirzakolonovazizbek@gmail.com>
2022-02-03 14:49:48 +0500

    Audit

---
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team2/configuration/AuditorAwareImpl.java
@@ -0,0 +1,16 @@
+package com.exadel.sandbox.team2.configuration;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuditorAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        String loggedInUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+        return Optional.of(loggedInUsername);
+    }
+}
---
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team2/domain/base/BaseEntity.java
@@ -0,0 +1,44 @@
+package com.exadel.sandbox.team2.domain.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@MappedSuperclass
+public class BaseEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    @CreatedBy
+    private String createdBy;
+
+    @Column
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @Column
+    @LastModifiedBy
+    private String modifiedBy;
+
+    @Column
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
