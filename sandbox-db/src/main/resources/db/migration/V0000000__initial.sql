eb7f878f9f6357c0d673e916fb66d8eb9641d7fb
Ilya Getman <vieletiger@gmail.com>
2022-02-03 18:43:36 +0300

    change sql script

--- sandbox-db/src/main/resources/db/migration/V0000000__initial.sql
+++ sandbox-db/src/main/resources/db/migration/V0000000__initial.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `User`
 (
-   id                   INT                        NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   chat_id              INT                        NOT NULL,
+   id                   BIGINT                     NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   chat_id              VARCHAR(50)                NOT NULL,
    first_name           VARCHAR(50)                NOT NULL,
    last_name            VARCHAR(50)                NOT NULL,
    email                VARCHAR(50)                NOT NULL,
