7a3afe78ce035ddbb17db76f902b4ce00a4c6c7a
Azizbek <mirzakolonovazizbek@gmail.com>
2022-02-06 20:24:10 +0500

    18-create-a-stub-for-security

--- sandbox-web/src/main/java/com/exadel/sandbox/team2/configuration/AppSecurityConfiguration.java
+++ sandbox-web/src/main/java/com/exadel/sandbox/team2/configuration/AppSecurityConfiguration.java
@@ -1,9 +1,6 @@
 package com.exadel.sandbox.team2.configuration;
 
-import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
