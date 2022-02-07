cf3389c8b9d61e69315e340e5ab025a6314ac867
divergenny <divergenny@gmail.com>
2022-02-07 23:10:05 +0500

    add lang property for telegram, rename package, add spring annotation Component to TelegramBotComponent.java, remove not necessary bean creation in Application.java, correct style of code.

--- sandbox-web/src/main/java/com/exadel/sandbox/team2/Application.java
+++ sandbox-web/src/main/java/com/exadel/sandbox/team2/Application.java
@@ -1,20 +1,12 @@
 package com.exadel.sandbox.team2;
 
-import com.exadel.sandbox.team2.component.TelegramBotComponent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Application {
 
-   public static void main(String[] args) {
-      SpringApplication.run(Application.class, args);
-   }
-
-   @Bean
-   public TelegramBotComponent getTelegramBotComponent(){
-      return new TelegramBotComponent();
-   }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
