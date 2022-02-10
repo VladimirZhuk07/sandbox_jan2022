c154f19973333f140bb967a9e10fe0feb9d09a8a
Ilya Getman <vieletiger@gmail.com>
2022-02-10 14:44:22 +0300

    change UserRestController

--- sandbox-web/src/main/java/com/exadel/sandbox/team2/controller/UserRestController.java
+++ sandbox-web/src/main/java/com/exadel/sandbox/team2/controller/UserRestController.java
@@ -3,7 +3,7 @@ package com.exadel.sandbox.team2.controller;
 import com.exadel.sandbox.team2.domain.User;
 import com.exadel.sandbox.team2.dto.UserDto;
 import com.exadel.sandbox.team2.mapper.UserMapper;
-import com.exadel.sandbox.team2.serivce.UserService;
+import com.exadel.sandbox.team2.serivce.CRUDService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,26 +16,26 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class UserRestController {
 
-    private final UserService service;
+    private final CRUDService<User> crudService;
 
     private final UserMapper mapper;
 
     @GetMapping("/{id}")
     public UserDto findById(@PathVariable Long id) {
-        Optional<User> user = service.findById(id);
+        Optional<User> user = crudService.findById(id);
         return mapper.toDto(user.get());
     }
 
     @GetMapping
     public List<UserDto> findAll() {
-        List<User> list = service.findAll();
+        List<User> list = crudService.findAll();
         return list.stream().map(mapper::toDto).collect(Collectors.toList());
     }
 
     @PostMapping
     public UserDto add(@RequestBody UserDto userDto) {
         User user = mapper.toEntity(userDto);
-        User newUser = service.save(user);
+        User newUser = crudService.save(user);
         return mapper.toDto(newUser);
     }
 
@@ -43,12 +43,12 @@ public class UserRestController {
     public UserDto update(@PathVariable Long id, @RequestBody UserDto userDto) {
         User newUser = mapper.toEntity(userDto);
         newUser.setId(id);
-        service.update(newUser);
+        crudService.update(newUser);
         return mapper.toDto(newUser);
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
-        service.delete(id);
+        crudService.delete(id);
     }
 }
