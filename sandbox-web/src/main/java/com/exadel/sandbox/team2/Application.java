86b9c557fd3b7230fb3b2dc9a291cf7f4a2772ca
Azizbek <mirzakolonovazizbek@gmail.com>
2022-02-11 15:30:44 +0500

    Commit for review

--- sandbox-web/src/main/java/com/exadel/sandbox/team2/Application.java
+++ sandbox-web/src/main/java/com/exadel/sandbox/team2/Application.java
@@ -1,15 +1,8 @@
 package com.exadel.sandbox.team2;
 
-import com.exadel.sandbox.team2.domain.Country;
-import com.exadel.sandbox.team2.domain.Map;
-import com.exadel.sandbox.team2.domain.Office;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @SpringBootApplication
 @EnableJpaAuditing
