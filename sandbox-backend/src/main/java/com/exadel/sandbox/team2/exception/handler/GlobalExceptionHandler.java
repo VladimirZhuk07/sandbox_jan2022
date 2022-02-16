14f542f71a92bf6dcd7a18ee2350c89d98f3947d
Ilya Getman <vieletiger@gmail.com>
2022-02-16 22:41:15 +0300

    refactor global exception handler

--- sandbox-backend/src/main/java/com/exadel/sandbox/team2/exception/handler/GlobalExceptionHandler.java
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team2/exception/handler/GlobalExceptionHandler.java
@@ -7,10 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 @Slf4j
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<ErrorData> handleAppException(AppException exception) {
