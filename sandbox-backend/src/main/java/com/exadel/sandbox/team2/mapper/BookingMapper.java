ce76ff07fd2c91c1949c7ab2809cc79c543e757a
Ilya Getman <vieletiger@gmail.com>
2022-02-16 21:26:15 +0300

    Corrected booking mapper

--- sandbox-backend/src/main/java/com/exadel/sandbox/team2/mapper/BookingMapper.java
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team2/mapper/BookingMapper.java
@@ -1,11 +1,22 @@
 package com.exadel.sandbox.team2.mapper;
 
 import com.exadel.sandbox.team2.domain.Booking;
+import com.exadel.sandbox.team2.domain.Workplace;
 import com.exadel.sandbox.team2.dto.BookingDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface BookingMapper {
+
+    @Mapping(source = "workplace", target = "workplaceId", qualifiedByName = "getWorkplaceId")
     BookingDto toDto(Booking booking);
+
     Booking toEntity(BookingDto bookingDto);
+
+    @Named("getWorkplaceId")
+    static Long getWorkplaceId(Workplace workplace) {
+        return workplace.getId();
+    }
 }
