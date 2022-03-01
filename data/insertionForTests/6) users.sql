1b6f1adef67ccd53e6d6835882455674351f95ec
divergenny <divergenny@gmail.com>
2022-03-02 00:53:12 +0500

    add: insertions for tests

--- data/insertionForTests/6) users.sql
+++ data/insertionForTests/6) users.sql
@@ -1,36 +1,83 @@
-insert into `sandbox`.`user`
-(`chatId`, `firstName`, `lastName`,
-`email`, `phoneNumber`, `employmentStart`,
-`employmentEnd`, `telegramAuthorizationCode`, `telegramState`,
-`status`, `createdDate`, `modifiedDate`,
-`createdBy`, `modifiedBy`, `isFired`)
-VALUES
-(12, 'Vladislav', 'Test2',
-'divergenny@gmail.com', 123121232,'2021.12.02',
-'2021.12.02', 12131234, null,
-'NEW', '2022.01.03', '2022.01.03',
-'vlad', 'vlad', false);
-
-insert into `sandbox`.`user`
-(`chatId`, `firstName`, `lastName`,
-`email`, `phoneNumber`, `employmentStart`,
-`employmentEnd`, `telegramAuthorizationCode`, `telegramState`,
-`status`, `createdDate`, `modifiedDate`,
-`createdBy`, `modifiedBy`, `isFired`)
-VALUES
-(12, 'Aizizbek', 'Test2',
-'mirzakolonovazizbek@gmail.com', 12312123,'2021.12.02',
-'2021.12.02', 121312344, null,
-'NEW', '2022.01.03', '2022.01.03',
-'vlad', 'vlad', false);
-
-insert into user (chatId, firstName,
+insert into user (id, chatId, firstName,
                   lastName, email, phoneNumber,
                   employmentStart, employmentEnd, telegramAuthorizationCode,
                   telegramState, status, createdDate,
-                  modifiedDate, createdBy, modifiedBy, isFired)
-values (null, 'Vladislav', 'Userovich', 'divergenny@gmail.com', '+998946622097',
-        '01.03.2022', null, null, null, 'new_user', NOW(), NOW(), 'admin', 'admin', false);
+                  modifiedDate, createdBy, modifiedBy,
+                  isFired)
+values (1, null, 'Vladislav',
+        'Userovich', 'divergenny@gmail.com', '+998946622097',
+        '01.03.2022', null, null,
+        null, 'new', NOW(),
+        NOW(), 'admin', 'admin',
+        false);
+
+INSERT INTO userrole (userId, roleId)
+VALUES (1, 1);
 
 
+insert into user (id, chatId, firstName,
+                  lastName, email, phoneNumber,
+                  employmentStart, employmentEnd, telegramAuthorizationCode,
+                  telegramState, status, createdDate,
+                  modifiedDate, createdBy, modifiedBy,
+                  isFired)
+values (2, null, 'Aizizbek',
+        'Systemovich', 'mirzakolonovazizbek@gmail.com', '+998998771917',
+        '01.03.2022', null, null,
+        null, 'new', NOW(),
+        NOW(), 'admin', 'admin',
+        false);
 
+INSERT INTO userrole (userId, roleId)
+VALUES (2, 2);
+
+
+insert into user (id, chatId, firstName,
+                  lastName, email, phoneNumber,
+                  employmentStart, employmentEnd, telegramAuthorizationCode,
+                  telegramState, status, createdDate,
+                  modifiedDate, createdBy, modifiedBy,
+                  isFired)
+values (3, null, 'Ilya',
+        'Mapeditorovich', 'divergenny@gmail.com', '++998946622197',
+        '01.03.2022', null, null,
+        null, 'new', NOW(),
+        NOW(), 'admin', 'admin',
+        false);
+
+INSERT INTO userrole (userId, roleId)
+VALUES (3, 3);
+
+
+insert into user (id, chatId, firstName,
+                  lastName, email, phoneNumber,
+                  employmentStart, employmentEnd, telegramAuthorizationCode,
+                  telegramState, status, createdDate,
+                  modifiedDate, createdBy, modifiedBy,
+                  isFired)
+values (4, null, 'Alena',
+        'Managerovich', 'mirzakolonovazizbek@gmail.com', '+998998721917',
+        '01.03.2022', null, null,
+        null, 'new', NOW(),
+        NOW(), 'admin', 'admin',
+        false);
+
+INSERT INTO userrole (userId, roleId)
+VALUES (4, 4);
+
+
+insert into user (id, chatId, firstName,
+                  lastName, email, phoneNumber,
+                  employmentStart, employmentEnd, telegramAuthorizationCode,
+                  telegramState, status, createdDate,
+                  modifiedDate, createdBy, modifiedBy,
+                  isFired)
+values (5, null, 'Vladimir',
+        'Administratorovich', 'divergenny@gmail.com', '+998998756090',
+        '01.03.2022', null, null,
+        null, 'new', NOW(),
+        NOW(), 'admin', 'admin',
+        false);
+
+INSERT INTO userrole (userId, roleId)
+VALUES (5, 5);
