ceb74a6f30c1ff62638fa62257960e600f65b0c9
Nurmukhammad Sunatullaev <92370241+nurmukhammadsunatullaev@users.noreply.github.com>
2022-03-01 23:04:27 +0500

    some buttons created

--- sandbox-telegram/src/main/java/com/exadel/sandbox/team2/service/TelegramAuthorizationService.java
+++ sandbox-telegram/src/main/java/com/exadel/sandbox/team2/service/TelegramAuthorizationService.java
@@ -20,68 +20,34 @@ public class TelegramAuthorizationService {
         this.userService = userService;
     }
 
-    public TelegramResponse<?> saveTelegramUserPhone(String chatId, String phoneNumber) {
-        Optional<User> optionalUser = userService.getUserByTelegramChatIdOrPhone(chatId, phoneNumber);
-        if (optionalUser.isEmpty()) {
-            return TelegramResponse.builder()
-                    .code(404)
-                    .message("User not found")
-                    .build();
+    public Optional<User> findActiveUserByChatId(String chatId){
+        return userService.findActiveUserByChatId(chatId);
+    }
+
+    public Optional<User> authenticatePhoneNumber(String chatId, String phoneNumber) {
+        Optional<User> current = userService.findUserByChatIdOrPhoneNumber(chatId, phoneNumber);
+        if(current.isPresent()){
+            User user = current.get();
+            if (Objects.equals(chatId, user.getChatId()) && user.getStatus() == UserState.WAIT_PHONE_AUTHORIZATION) {
+                user.setStatus(UserState.ACTIVE);
+                user.setPhoneNumber(phoneNumber);
+            } else {
+                user.setChatId(chatId);
+                user.setStatus(UserState.NEW);
+            }
+            return Optional.of(userService.save(user));
         }
+        return Optional.empty();
+    }
 
-        User user = optionalUser.get();
-
-        if (Objects.equals(chatId, user.getChatId()) && user.getStatus() == UserState.WAIT_PHONE_AUTHORIZATION) {
-            user.setStatus(UserState.ACTIVE);
-            user.setPhoneNumber(phoneNumber);
-            userService.save(user);
-            return TelegramResponse.builder()
-                    .code(200)
-                    .message("Successfully user activated")
-                    .build();
-        } else if (Objects.equals(phoneNumber, user.getPhoneNumber())) {
+    public Optional<User> authenticate(String chatId, String code) {
+        Optional<User> current = userService.findInvitedUserByAuthorizationCode(code);
+        if(current.isPresent()){
+            User user = current.get();
             user.setChatId(chatId);
-            user.setStatus(UserState.NEW);
-            userService.save(user);
-            return TelegramResponse.builder()
-                    .code(201)
-                    .message("Please wait invitation via email")
-                    .build();
+            user.setStatus(UserState.WAIT_PHONE_AUTHORIZATION);
+            return Optional.of(userService.save(user));
         }
-        return TelegramResponse.builder()
-                .code(500)
-                .message("Some error")
-                .build();
-
+        return Optional.empty();
     }
-
-    public TelegramResponse<?> authorizationTelegramUser(String code, String chatId) {
-        Optional<User> optionalUser = userService.getUserByAuthorizationCode(code);
-        if (optionalUser.isEmpty()) {
-            return TelegramResponse.builder()
-                    .code(404)
-                    .message("User not found")
-                    .build();
-        }
-
-        User user = optionalUser.get();
-
-        if (user.getStatus() != UserState.INVITED) {
-            return TelegramResponse.builder()
-                    .code(405)
-                    .message("Expired authorization code")
-                    .build();
-        }
-
-        user.setChatId(chatId);
-        user.setStatus(UserState.WAIT_PHONE_AUTHORIZATION);
-        userService.save(user);
-
-        return TelegramResponse.builder()
-                .code(200)
-                .message("User successfully updated")
-                .build();
-    }
-
-
 }
--- sandbox-telegram/src/main/java/com/exadel/sandbox/team2/service/TelegramMessageService.java
+++ sandbox-telegram/src/main/java/com/exadel/sandbox/team2/service/TelegramMessageService.java
@@ -1,5 +1,7 @@
 package com.exadel.sandbox.team2.service;
 
+import com.exadel.sandbox.team2.domain.User;
+import com.exadel.sandbox.team2.domain.enums.UserState;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -11,7 +13,10 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -22,7 +27,53 @@ public class TelegramMessageService {
     TelegramAuthorizationService authorizationService;
 
     public SendMessage handleUpdate(Update update) {
+
+        if(!update.hasMessage()){
+            return null;
+        }
+
         String chatId=update.getMessage().getChatId().toString();
+        Optional<User> current = authorizationService.findActiveUserByChatId(chatId);
+
+        if(current.isEmpty()) {
+            if (update.getMessage().hasText()) {
+                String command = update.getMessage().getText().trim();
+
+                if (command.equals("/start")) {
+                    return requestPhoneNumber(chatId, "Please send your phone number for invitation!");
+                }
+                else if (command.startsWith("/start")) {
+                    String authorizationCode = command.substring("/start".length()).trim();
+                    var response = authorizationService.authenticate(chatId, authorizationCode);
+                    if(response.isPresent()){
+                      return requestPhoneNumber(chatId, "Please send your phone number for authentication");
+                    }
+                    return SendMessage.builder()
+                            .chatId(chatId)
+                            .text("Sorry you are not use this bot")
+                            .build();
+                }
+            }
+            else {
+                Contact contact = update.getMessage().getContact();
+                var response = authorizationService.authenticatePhoneNumber(chatId, contact.getPhoneNumber());
+                if(response.isPresent()){
+                    User user = response.get();
+                    switch (user.getStatus()){
+                        case NEW : return SendMessage.builder()
+                                .chatId(chatId)
+                                .text("We sent invitation your email")
+                                .build();
+                        case ACTIVE: return SendMessage.builder()
+                                .chatId(chatId)
+                                .text("Please select your function")
+                                .replyMarkup(createReplyKeyboardMarkup(new String[][]{{"\uD83C\uDF0E Language","⚙️ Settings"},{"Booking","History"}}))
+                                .build();
+                    }
+                }
+
+            }
+        }
         if(update.hasCallbackQuery()){
             return SendMessage.builder()
                               .chatId(update.getMessage()
@@ -31,70 +82,31 @@ public class TelegramMessageService {
                               .text("Please select command")
                               .build();
         }
-        else if(update.hasMessage() && !update.getMessage().hasText()){
-            Contact contact=update.getMessage().getContact();
-            var response = authorizationService.saveTelegramUserPhone(chatId, contact.getPhoneNumber());
 
-            if(response.getCode() == 200){
+        return SendMessage.builder()
+                .chatId(chatId)
+                .text("Empty")
+                .build();
 
-
-                ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-                replyKeyboardMarkup.setSelective(true);
-                replyKeyboardMarkup.setResizeKeyboard(true);
-                replyKeyboardMarkup.setOneTimeKeyboard(true);
-                replyKeyboardMarkup.setKeyboard(List.of(new KeyboardRow(List.of(
-                        createReplyButton("Book place >",false),
-                        createReplyButton("Show my booking >",false),
-                        createReplyButton("Settings >",false)
-                ))));
-                       return SendMessage.builder()
-                               .chatId(chatId)
-                               .text("Select action")
-                               .replyMarkup(replyKeyboardMarkup)
-                               .build();
-
-            }
-
-            if(response.getCode() == 201){
-
-
-            }
-            return SendMessage.builder()
-                              .chatId(update.getMessage()
-                                            .getChatId()
-                                            .toString())
-                              .text("Thanks for your contact: ".concat(response.getMessage()))
-                              .build();
-        }
-        else if (update.hasMessage() && update.getMessage().hasText()) {
-            String command = update.getMessage().getText().trim();
-
-            if(command.equals("/start")){
-                return requestPhoneNumber(chatId, "Please send your phone number");
-            }
-            else if(command.startsWith("/start")){
-                String authorizationCode = command.substring("/start".length()).trim();
-                var response= authorizationService.authorizationTelegramUser(authorizationCode,chatId);
-                if(response.getCode() == 200){
-                    return requestPhoneNumber(chatId,"Please send your phone number "+ authorizationCode);
-                }
-
-                return  SendMessage
-                  .builder()
-                  .chatId(chatId)
-                  .text("You are not Exadel member so you can not use this bot")
-                  .build();
-
-            }
-            return SendMessage
-                            .builder()
-                            .chatId(chatId)
-                            .text("Please select command")
-                            .build();
-        }
-        return null;
     }
 
+
+    public ReplyKeyboardMarkup createReplyKeyboardMarkup(String [][] titles){
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+        List<KeyboardRow> rows=new ArrayList<>();
+        for (int i = 0; i < titles.length; i++) {
+            List<KeyboardButton> buttons=new ArrayList();
+            for (int j = 0; j < titles[i].length; j++) {
+                buttons.add(createReplyButton(titles[i][j],false));
+            }
+            rows.add(new KeyboardRow(buttons));
+        }
+        replyKeyboardMarkup.setKeyboard(rows);
+        return replyKeyboardMarkup;
+    }
 
     private SendMessage requestPhoneNumber(String chatId,String message){
 
@@ -112,14 +124,10 @@ public class TelegramMessageService {
                             .build();
     }
 
-
-
     private KeyboardButton createReplyButton(String buttonText, boolean requestContact){
         return KeyboardButton.builder()
                                 .text(buttonText)
                                 .requestContact(requestContact)
                              .build();
     }
-
-
 }
