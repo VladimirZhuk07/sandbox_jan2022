e4fef82da6780694d78fe83fbaa6143043b478c3
Nurmukhammad Sunatullaev <nurmukhammad.sunatullaev@gmail.com>
2022-03-02 14:24:22 +0500

    InlineButtons

--- sandbox-telegram/src/main/java/com/exadel/sandbox/team2/service/TelegramMessageService.java
+++ sandbox-telegram/src/main/java/com/exadel/sandbox/team2/service/TelegramMessageService.java
@@ -10,10 +10,13 @@ import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Contact;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -27,12 +30,7 @@ public class TelegramMessageService {
     TelegramAuthorizationService authorizationService;
 
     public SendMessage handleUpdate(Update update) {
-
-        if(!update.hasMessage()){
-            return null;
-        }
-
-        String chatId=update.getMessage().getChatId().toString();
+        String chatId=getChatId(update);
         Optional<User> current = authorizationService.findActiveUserByChatId(chatId);
 
         if(current.isEmpty()) {
@@ -46,12 +44,12 @@ public class TelegramMessageService {
                     String authorizationCode = command.substring("/start".length()).trim();
                     var response = authorizationService.authenticate(chatId, authorizationCode);
                     if(response.isPresent()){
-                      return requestPhoneNumber(chatId, "Please send your phone number for authentication");
+                        return requestPhoneNumber(chatId, "Please send your phone number for authentication");
                     }
                     return SendMessage.builder()
-                            .chatId(chatId)
-                            .text("Sorry you are not use this bot")
-                            .build();
+                      .chatId(chatId)
+                      .text("Sorry you are not use this bot")
+                      .build();
                 }
             }
             else {
@@ -59,28 +57,35 @@ public class TelegramMessageService {
                 var response = authorizationService.authenticatePhoneNumber(chatId, contact.getPhoneNumber());
                 if(response.isPresent()){
                     User user = response.get();
-                    switch (user.getStatus()){
-                        case NEW : return SendMessage.builder()
-                                .chatId(chatId)
-                                .text("We sent invitation your email")
-                                .build();
-                        case ACTIVE: return SendMessage.builder()
-                                .chatId(chatId)
-                                .text("Please select your function")
-                                .replyMarkup(createReplyKeyboardMarkup(new String[][]{{"\uD83C\uDF0E Language","⚙️ Settings"},{"Booking","History"}}))
-                                .build();
+                    if (user.getStatus() == UserState.NEW) {
+                        return SendMessage.builder()
+                          .chatId(chatId)
+                          .text("We sent invitation your email")
+                          .build();
+                    } else if (user.getStatus() == UserState.ACTIVE) {
+                        return SendMessage.builder()
+                          .chatId(chatId)
+                          .text("Please select your function")
+                          .replyMarkup(createInlineKeyboardMarkup(new String[][]{{"\uD83C\uDF0E Language", "⚙️ Settings"}, {"Booking", "History"}},new String[][]{{"\uD83C\uDF0E Language", "⚙️ Settings"}, {"Booking", "History"}}))
+                          //.replyMarkup(createReplyKeyboardMarkup(new String[][]{{"\uD83C\uDF0E Language", "⚙️ Settings"}, {"Booking", "History"}}))
+                          .build();
                     }
                 }
 
             }
         }
+
+
+
+
         if(update.hasCallbackQuery()){
             return SendMessage.builder()
-                              .chatId(update.getMessage()
-                                            .getChatId()
-                                            .toString())
-                              .text("Please select command")
-                              .build();
+
+              .chatId(update.getMessage()
+                .getChatId()
+                .toString())
+              .text("Please select command")
+              .build();
         }
 
         return SendMessage.builder()
@@ -90,15 +95,40 @@ public class TelegramMessageService {
 
     }
 
+    private String getChatId(Update update){
+        if(update.hasCallbackQuery()){
+            return update.getCallbackQuery().getMessage().getChatId().toString();
+        }
+        return update.getMessage().getChatId().toString();
+    }
 
-    public ReplyKeyboardMarkup createReplyKeyboardMarkup(String [][] titles){
+    private InlineKeyboardMarkup createInlineKeyboardMarkup(String [][] titles,String[][] commands) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+
+        for (int i = 0; i < titles.length; i++) {
+            List<InlineKeyboardButton> row = new ArrayList<>();
+            for (int j = 0; j < titles[i].length; j++) {
+                row.add(
+                  InlineKeyboardButton.builder()
+                    .text(titles[i][j])
+                    .callbackData(commands[i][j])
+                    .build());
+            }
+            rows.add(row);
+        }
+        inlineKeyboardMarkup.setKeyboard(rows);
+        return inlineKeyboardMarkup;
+    }
+
+    private ReplyKeyboardMarkup createReplyKeyboardMarkup(String [][] titles){
         ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
         replyKeyboardMarkup.setSelective(true);
         replyKeyboardMarkup.setResizeKeyboard(true);
         replyKeyboardMarkup.setOneTimeKeyboard(true);
         List<KeyboardRow> rows=new ArrayList<>();
         for (int i = 0; i < titles.length; i++) {
-            List<KeyboardButton> buttons=new ArrayList();
+            List<KeyboardButton> buttons=new ArrayList<>();
             for (int j = 0; j < titles[i].length; j++) {
                 buttons.add(createReplyButton(titles[i][j],false));
             }
