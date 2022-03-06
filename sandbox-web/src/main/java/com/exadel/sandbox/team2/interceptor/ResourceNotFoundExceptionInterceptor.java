f8e038ce3ead0f40869e2bcd84df1593bbafcdc7
divergenny <divergenny@gmail.com>
2022-03-06 14:17:07 +0500

    fix: remove unnecessary methods and shortened the code

--- sandbox-web/src/main/java/com/exadel/sandbox/team2/interceptor/ResourceNotFoundExceptionInterceptor.java
+++ sandbox-web/src/main/java/com/exadel/sandbox/team2/interceptor/ResourceNotFoundExceptionInterceptor.java
@@ -12,49 +12,9 @@ import javax.persistence.EntityNotFoundException;
 @Component
 public class ResourceNotFoundExceptionInterceptor {
 
-    @AfterThrowing(value = "execution(* com.exadel.sandbox.team2.controller.UserRestController.*(..))",
+    @AfterThrowing(value = "execution(* com.exadel.sandbox.team2.controller.*.*(..))",
             throwing = "e")
-    public void errorInterceptorForUserRestController(JoinPoint thisJoinPoint, Throwable e) {
-        if (e instanceof EntityNotFoundException) {
-            throw new ResourceNotFoundException("Resource was not found: " + thisJoinPoint.getTarget().getClass());
-        }
-    }
-
-    @AfterThrowing(value = "execution(* com.exadel.sandbox.team2.controller.BookingRestController.*(..))",
-            throwing = "e")
-    public void errorInterceptorForBookingRestController(JoinPoint thisJoinPoint, Throwable e) {
-        if (e instanceof EntityNotFoundException) {
-            throw new ResourceNotFoundException("Resource was not found: " + thisJoinPoint.getTarget().getClass());
-        }
-    }
-
-    @AfterThrowing(value = "execution(* com.exadel.sandbox.team2.controller.CountryRestController.*(..))",
-            throwing = "e")
-    public void errorInterceptorForCountryRestController(JoinPoint thisJoinPoint, Throwable e) {
-        if (e instanceof EntityNotFoundException) {
-            throw new ResourceNotFoundException("Resource was not found: " + thisJoinPoint.getTarget().getClass());
-        }
-    }
-
-    @AfterThrowing(value = "execution(* com.exadel.sandbox.team2.controller.MapRestController.*(..))",
-            throwing = "e")
-    public void errorInterceptorForMapRestController(JoinPoint thisJoinPoint, Throwable e) {
-        if (e instanceof EntityNotFoundException) {
-            throw new ResourceNotFoundException("Resource was not found: " + thisJoinPoint.getTarget().getClass());
-        }
-    }
-
-    @AfterThrowing(value = "execution(* com.exadel.sandbox.team2.controller.OfficeRestController.*(..))",
-            throwing = "e")
-    public void errorInterceptorForOfficeRestController(JoinPoint thisJoinPoint, Throwable e) {
-        if (e instanceof EntityNotFoundException) {
-            throw new ResourceNotFoundException("Resource was not found: " + thisJoinPoint.getTarget().getClass());
-        }
-    }
-
-    @AfterThrowing(value = "execution(* com.exadel.sandbox.team2.controller.WorkplaceRestController.*(..))",
-            throwing = "e")
-    public void errorInterceptorForWorkplaceRestController(JoinPoint thisJoinPoint, Throwable e) {
+    public void errorInterceptorForWebRestControllers(JoinPoint thisJoinPoint, Throwable e) {
         if (e instanceof EntityNotFoundException) {
             throw new ResourceNotFoundException("Resource was not found: " + thisJoinPoint.getTarget().getClass());
         }
